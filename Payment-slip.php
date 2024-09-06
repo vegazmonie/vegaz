72dc5f56707065e8d05279421ae3af8d8bd68c19
vegazmonie <babatundeosibogun@gmail.com>
2024-09-06 15:20:29 +0400

    Add files via upload

---
+++ Payment-slip.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="veegaz.css">
+</head>
+<body>
+    <form action="vegaz.php">
+        <input type="file" placeholder="Payment-proof #"> <br>
+        <br>
+        <a class="pop" href="">Submit</a>
+    </form>
+    
+</body>
+</html>
